18452e75a18d5a55a096336d6132c97a34bd4659
Sergei Elizarov <ser.elizarow2015@yandex.ru>
2018-08-27 18:58:46 +0300

    -'perv'

--- DZ3/index.php
+++ DZ3/index.php
@@ -1,1 +1,14 @@
+<pre>
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require('src/functions.php');
+echo '<h2>Задание №1</h2>';
+echo task1();
+echo '<h2>Задание №2</h2>';
+echo task2();
+echo '<h2>Задание №3</h2>';
+echo task3();
+echo '<h2>Задание №4</h2>';
+echo task4();
