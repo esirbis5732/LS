21fbc96de5bc825a04283d6ee748cabf40350fd8
Sergei Elizarov <ser.elizarow2015@yandex.ru>
2018-08-27 20:12:59 +0300

    -'perv'

--- DZ3/src/functions.php
+++ DZ3/src/functions.php
@@ -3,4 +3,15 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+function task1()
+{
+    $xmlFileName = __DIR__ . DIRECTORY_SEPARATOR . 'data.xml';
+    $xml = simplexml_load_file($xmlFileName);
+    if ($xml === false) {
+        return null;
+    }
+    // Сюда попали - значит xml распарсился
+    // Подключаем...
+    include 'order_blank.php';
+}
 
