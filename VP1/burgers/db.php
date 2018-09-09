6e9e196c57e5adaa6711766b770ba6a3316de5d6
Sergei Elizarov <ser.elizarow2015@yandex.ru>
2018-09-09 12:28:17 +0300

    init

--- VP1/burgers/db.php
+++ VP1/burgers/db.php
@@ -1,20 +1,21 @@
 <?php
 // Модуль для подключения к базе
 // Файл с параметрами подключения
-$config = include_once 'config.php';
+/*$config = include_once 'config.php';
 foreach ($config['db'] as $key => $value) {
     ${$key} = $value;
 }
-try {
-    $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset utf-8";
-    $opt = [
+try {*/
+    $dsn = "mysql:host=localhost;charset=$charset utf-8";
+  /*  $opt = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false
-    ];
+    ];*/
     // Подключаемся к базе
-    $dbh = new PDO($dsn, $user, $password, $opt);
+    $dbh = new PDO($dsn,root, ' ');
     return $dbh;
-} catch (PDOException $e) {
+//}
+/*catch (PDOException $e) {
     return false;
-}
+}*/
