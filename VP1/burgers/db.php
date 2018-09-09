f467818aca148bc5aee9178509c89ec9d01d81e0
Sergei Elizarov <ser.elizarow2015@yandex.ru>
2018-09-09 12:42:18 +0300

    init

--- VP1/burgers/db.php
+++ VP1/burgers/db.php
@@ -7,11 +7,11 @@ foreach ($config['db'] as $key => $value) {
 }
 try {*/
     $dsn = "mysql:host=localhost;charset=$charset utf-8";
-  /*  $opt = [
+  $opt = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false
-    ];*/
+    ];
     // Подключаемся к базе
     $dbh = new PDO($dsn,'root',' ' );
     return $dbh;
